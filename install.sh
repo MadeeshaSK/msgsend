365b3cce7ccb4e29365362a6ab26c4ec294656fa
MadeeshaSK <madeeshasachindu2@gmail.com>
2025-07-22 18:04:35 +0530

    release url fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Downloading msgsend v2.0..."
-wget -O msgsend https://github.com/MadeeshaSK/msgsend/releases/tag/v2.0
+wget -O msgsend https://github.com/MadeeshaSK/msgsend/releases/download/v2.0/msgsend
 
 if [ $? -eq 0 ]; then
     chmod +x msgsend
